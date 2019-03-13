db709135d47227707a4fba7e2d25ba2ea22e8ef9
AlbinoDrought <albinodrought@gmail.com>
2019-03-12 23:48:43 -0700

    feat: first partially-working file+creamy-format/format to ffmpeg command to transcoded video

---
+++ transcoder/format_to_ffmpeg.go
@@ -0,0 +1,89 @@
+package transcoder
+
+import (
+	"fmt"
+
+	cfmt "github.com/AlbinoDrought/creamy-transcode/format"
+)
+
+// FormatToFFMPEG converts a Format to one or more ffmpeg commands
+func FormatToFFMPEG(format cfmt.Format) [][]string {
+	result := []string{}
+
+	result = append(result, "-y", "-f", format.Container)
+
+	if format.VideoSpecs.Disabled {
+		result = append(result, "-vn")
+	} else {
+		// video resolution
+		if format.VideoSpecs.ResolutionHeight != 0 || format.VideoSpecs.ResolutionWidth != 0 {
+			height := format.VideoSpecs.ResolutionHeight
+			width := format.VideoSpecs.ResolutionWidth
+
+			// -1 signifies automatic in ffmpeg
+			if height == 0 {
+				height = -1
+			} else if width == 0 {
+				width = -1
+			}
+
+			result = append(result, "-vf", fmt.Sprintf("scale=%d:%d", width, height))
+		}
+
+		// video bitrate
+		if format.VideoSpecs.BitrateKbps != 0 {
+			bitrate := fmt.Sprintf("%dk", format.VideoSpecs.BitrateKbps)
+			result = append(result, "-b:v", bitrate, "-bufsize", bitrate)
+		}
+
+		// video codec
+		if format.VideoSpecs.Codec != "" {
+			result = append(result, "-codec:v", format.VideoSpecs.Codec)
+		}
+
+		// video fps
+		if format.VideoSpecs.FPS != "" && format.VideoSpecs.FPS != cfmt.FPSAutomatic {
+			result = append(result, "-r", format.VideoSpecs.FPS)
+		}
+	}
+
+	if format.AudioSpecs.Disabled {
+		result = append(result, "-an")
+	} else {
+		// audio cdoec
+		if format.AudioSpecs.Codec != "" {
+			result = append(result, "-codec:a", format.AudioSpecs.Codec)
+		}
+
+		// audio bitrate
+		if format.AudioSpecs.BitrateKbps != 0 {
+			result = append(result, "-b:a", fmt.Sprintf("%dk", format.AudioSpecs.BitrateKbps))
+		}
+
+		// audio sample rate
+		if format.AudioSpecs.SampleRateHz != 0 {
+			result = append(result, "-ar", fmt.Sprintf("%d", format.AudioSpecs.SampleRateHz))
+		}
+
+		// audio channel (mono, stereo)
+		if format.AudioSpecs.AudioChannel == cfmt.AudioChannelMono {
+			result = append(result, "-ac", "1")
+		} else if format.AudioSpecs.AudioChannel == cfmt.AudioChannelStereo {
+			result = append(result, "-ac", "2")
+		}
+	}
+
+	commands := [][]string{}
+
+	// experimental
+	if format.FormatOptions.TwoPass {
+		firstPass := append(result, "-pass", "1")
+		secondPass := append(result, "-pass", "2")
+
+		commands = append(commands, firstPass, secondPass)
+	} else {
+		commands = append(commands, result)
+	}
+
+	return commands
+}
---
+++ transcoder/transcode.go
@@ -0,0 +1,52 @@
+package transcoder
+
+import (
+	"os/exec"
+
+	cfmt "github.com/AlbinoDrought/creamy-transcode/format"
+	"github.com/pkg/errors"
+)
+
+// TranscodeFormat takes the file at `input` and transcodes it using the given Format,
+// saving it at `output`
+func TranscodeFormat(input string, output string, format cfmt.Format) error {
+	commands := FormatToFFMPEG(format)
+	return TranscodeRaw(input, output, commands)
+}
+
+// TranscodeRaw takes the file at `input` and transcodes it using the given raw commands,
+// saving it at `output`
+func TranscodeRaw(input string, output string, commands [][]string) error {
+	for _, command := range commands {
+		fullArgs := []string{
+			"-i",
+			input,
+		}
+
+		fullArgs = append(fullArgs, command...)
+		fullArgs = append(fullArgs, "-strict", "-2", output)
+
+		cmd := exec.Command("ffmpeg", fullArgs...)
+		output, err := cmd.Output()
+
+		if err != nil {
+			exitError := err.(*exec.ExitError)
+			errorOutput := ""
+			if exitError != nil {
+				errorOutput = string(exitError.Stderr)
+			}
+
+			return errors.Wrapf(
+				err,
+				"input: %+v\noutput: %+v\n command: %+v\n output: %+v\nerror output: %+v\n",
+				input,
+				output,
+				command,
+				string(output),
+				errorOutput,
+			)
+		}
+	}
+
+	return nil
+}
